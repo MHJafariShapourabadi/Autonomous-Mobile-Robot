5df2d32f496537edc148179f610026c35a7b4270
Mohammadhossein Jafari Shapourabadi <mh.jafary.s@gmail.com>
2025-12-06 02:44:34 +0330

    add ekf node from robot_localization pkg
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/mobile_robot_bringup/launch/mobile_robot_gz_sim.launch.py
    	modified:   src/mobile_robot_description/urdf/imu_1.urdf.xacro
    	new file:   src/mobile_robot_localization/config/ekf_config.yaml
    	modified:   src/mobile_robot_localization/launch/mobile_robot_localization.launch.py
    	modified:   src/mobile_robot_localization/package.xml

--- src/mobile_robot_bringup/launch/mobile_robot_gz_sim.launch.py
+++ src/mobile_robot_bringup/launch/mobile_robot_gz_sim.launch.py
@@ -122,17 +122,7 @@ def generate_launch_description():
         }]
     )
 
-    imu_frame_id_converter = Node(
-        package="mobile_robot_bringup",
-        executable="imu_frame_id_converter",
-        name="imu_frame_id_converter",
-        parameters=[{
-            "use_sim_time": True,
-            "frame_id": "base_footprint_ekf",
-            "subscription_topic": "imu/data_raw",
-            "publisher_topic": "imu_ekf",
-        }]
-    )
+    
 
     mobile_robot_controller = TimerAction(
         period=15.0,  # Wait 15 seconds for Gazebo + robot to initialize
@@ -204,7 +194,6 @@ def generate_launch_description():
         ros_gz_bridge,
         lidar_frame_id_converter,
         depth_camera_frame_id_converter,
-        imu_frame_id_converter,
         mobile_robot_controller,
         mobile_robot_localization,
         # start_object_detector,
--- src/mobile_robot_localization/launch/mobile_robot_localization.launch.py
+++ src/mobile_robot_localization/launch/mobile_robot_localization.launch.py
@@ -1,7 +1,8 @@
 from launch import LaunchDescription, LaunchContext
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
@@ -15,6 +16,15 @@ def generate_launch_description():
         variable_name="use_python"
     )
 
+    use_custom_sensor_fusion_arg = DeclareLaunchArgument(
+        name="use_custom_sensor_fusion",
+        default_value="False"
+    )
+
+    use_custom_sensor_fusion = LaunchConfiguration(
+        variable_name="use_custom_sensor_fusion"
+    )
+
     kalman_filter_py = Node(
         package="mobile_robot_localization",
         executable="kalman_filter.py",
@@ -26,9 +36,62 @@ def generate_launch_description():
         executable="kalman_filter",
         condition=UnlessCondition(use_python),
     )
+
+    custom_sensor_fusion = GroupAction(
+        actions=[
+            kalman_filter_py,
+            kalman_filter_cpp
+        ],
+        condition=IfCondition(use_custom_sensor_fusion)
+    )
+
+    ekf_static_transform_publisher = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=["--x", "0", "--y", "0", "--z", "0",
+                   "--qx", "0", "--qy", "0", "--qz", "0", "--qw", "1",
+                   "--frame-id", "base_footprint_ekf",
+                   "--child-frame-id", "imu_link_ekf"]
+    )
+
+    ekf_imu_frame_id_converter = Node(
+        package="mobile_robot_bringup",
+        executable="imu_frame_id_converter",
+        name="imu_frame_id_converter",
+        parameters=[{
+            "use_sim_time": True,
+            "frame_id": "base_footprint_ekf",
+            "subscription_topic": "imu/data_raw",
+            "publisher_topic": "imu/ekf",
+        }]
+    )
+
+    ekf_config = PathJoinSubstitution([
+        FindPackageShare("mobile_robot_localization"),
+        "config",
+        "ekf_config.yaml"
+    ])
+
+    ekf_filter = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[ekf_config]
+    )
+
+    ekf_sensor_fusion = GroupAction(
+        actions=[
+            ekf_static_transform_publisher,
+            ekf_imu_frame_id_converter,
+            ekf_filter,            
+        ],
+        condition=UnlessCondition(use_custom_sensor_fusion)
+    )
     
     return LaunchDescription([
         use_python_arg,
-        kalman_filter_py,
-        kalman_filter_cpp
+        use_custom_sensor_fusion_arg,
+        custom_sensor_fusion,
+        ekf_sensor_fusion,
     ])
